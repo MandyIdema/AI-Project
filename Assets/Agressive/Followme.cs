0530f413655a6e1b15dde0d1700cda2a9c108fde
Mandy Idema <m.e.idema@st.hanze.nl>
2023-03-07 15:20:50 +0100

    Added AI behaviour
    - AI now wanders around randomly
    - Agressive behaviour towards passive objects

---
+++ Assets/Agressive/Followme.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Followme : MonoBehaviour
+{
+    public GameObject passive;
+    public float speed;
+
+    private float distance;
+    public float distanceBetween;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
